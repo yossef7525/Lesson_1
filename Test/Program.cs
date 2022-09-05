16ae091941dda60bc8661da5514e83d04ca58f48
yossef7525 <a0548475725@gmail.com>
2022-09-05 18:04:52 +0300

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("please enter a day number");
+Console.ForegroundColor = ConsoleColor.White;
+string? day = Console.ReadLine();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("please enter a month number");
+Console.ForegroundColor = ConsoleColor.White;
+string? month = Console.ReadLine();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("please enter a year number");
+Console.ForegroundColor = ConsoleColor.White;
+string? year = Console.ReadLine();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(day + "/" + month + "/" + year);
+Console.ForegroundColor = ConsoleColor.White;
